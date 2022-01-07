194721db7f15fc03f95b93e6666194bc9009caf4
Chris Joakim <christopher.joakim@gmail.com>
2022-01-07 10:20:38 -0500

    initial files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # azure-jupyter
+
 jupyter
+
+## Links
+
+- https://code.visualstudio.com/docs/datascience/jupyter-notebooks
+- https://the9090rule.medium.com/loading-cosmos-db-graph-with-data-885f395288f0
+
+### Unused Libraries
+
+- https://ipython-gremlin.readthedocs.io/en/latest/index.html
+  - this is old, current version is 1.0.0 from March 2017
+
+- https://github.com/aws/graph-notebook
+  - AWS Neptune oriented
+
---
+++ cosmos-gremlin.py
@@ -0,0 +1,45 @@
+
+import os
+import sys
+import traceback
+
+from gremlin_python.driver import client, serializer, protocol
+from gremlin_python.driver.protocol import GremlinServerError
+
+
+if __name__ == "__main__":
+    try:
+        acct   = os.environ['AZURE_COSMOSDB_GRAPHDB_ACCT']
+        key    = os.environ['AZURE_COSMOSDB_GRAPHDB_KEY']
+        dbname = os.environ['AZURE_COSMOSDB_GRAPHDB_DBNAME']
+        graph  = os.environ['AZURE_COSMOSDB_GRAPHDB_GRAPH']
+
+        dbname = 'december'
+        graph  = 'rdf1'
+        url    = 'wss://{}.gremlin.cosmosdb.azure.com:443/'.format(acct)
+        username = 'dbs/{}/colls/{}'.format(dbname, graph)
+
+        print('acct: {}'.format(acct))
+        print('key:  {}'.format(key))
+        print('url:  {}'.format(url))
+        print('username: {}'.format(username))
+
+        client = client.Client(url, 'g', username=username, password=key,
+            message_serializer=serializer.GraphSONSerializersV2d0())
+
+        query = 'g.V().count()'
+        callback = client.submitAsync(query)
+        if callback.result() is not None:
+            print(callback)
+            result = callback.result()
+            attrs  = result.status_attributes
+            print(attrs)
+            print("count: {0}".format(callback.result().all().result()))
+            #print("count: {0}".format(callback.result().all().result())) 
+        else:
+            print("unable to execute query: {0}".format(query))
+
+        client.close()
+    except GremlinServerError as e:
+        print(e)
+        print('Code: {0}, Attributes: {1}'.format(e.status_code, e.status_attributes))
