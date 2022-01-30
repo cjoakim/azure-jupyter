498c91efa8ed7c7e366d7de056e214be537b8a19
Chris Joakim <christopher.joakim@gmail.com>
2022-01-30 16:44:09 -0500

    added jypyter_start.ps, added Seaborn to requirements

---
+++ jupyter_start.ps1
@@ -0,0 +1,9 @@
+
+# List the available kernels, then start jupyter.
+# Chris Joakim, Microsoft, January 2022
+
+echo 'listing available kernels ...'
+jupyter kernelspec list
+
+echo 'starting jupyter notebook ...'
+jupyter notebook
